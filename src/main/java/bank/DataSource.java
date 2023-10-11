0e59623f76856bbbedcf972c03fc1b63cbcea8cd
Zombiexl <122053932+Zombiexl@users.noreply.github.com>
2023-10-11 18:04:04 +0000

    Add getCustomer to DataSource

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -1,6 +1,8 @@
 package bank;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -10,7 +12,6 @@ public class DataSource {
     Connection connection = null;
     try {
       connection = DriverManager.getConnection(db_file);
-      System.out.println("we're connected");
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -18,7 +19,33 @@ public class DataSource {
     return connection;
   }
 
+  public static Customer getCustomer(String username) {
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+
+    try(Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+    
+          statement.setString(1, username);
+    
+          try(ResultSet resultSet = statement.executeQuery()) {
+            customer = new Customer(
+              resultSet.getInt("id"),
+              resultSet.getString("name"),
+              resultSet.getString("username"),
+              resultSet.getString("password"),
+              resultSet.getInt("account_id"));
+          }
+    } 
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
+    
+    return customer;
+  }
+
   public static void main(String[] args) {
-    connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
   }
 }
